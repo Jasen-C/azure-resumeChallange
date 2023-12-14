ff095f93709dddd93d71ef5ebee91da1f421d532
Jasen-C <68570791+Jasen-C@users.noreply.github.com>
2023-12-13 17:44:49 -0700

    updated main.js
    updated main.js with function app url and modified as needed.

--- frontend/main.js
+++ frontend/main.js
@@ -2,16 +2,18 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApiUrl = 'https://getresumecounter.azurewebsites.net/api/GetResumeCounter?code=Q/LkPt0mhQKdP8DoE3DdllAYdFQO//58Iq7AoS6JUBj9FgJ86Rqt8A==';
-const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://azure-crispsec-resume-counter-powershell.azurewebsites.net/api/counterHTTPtrigger';
+
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApiUrl).then(response => {
-        return response.json()
+        return response
+        // return response.json()
     }).then(response =>{
         console.log("Website called function API.");
-        count =  response.count;
+        count =  response;
+        // count =  response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
